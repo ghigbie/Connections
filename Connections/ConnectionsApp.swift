feebb7774e853ce88e059d8730350a30ff2f6ab6
ghigbie <gehigbie@gmail.com>
2020-12-16 20:42:57 -0500

    Initial commit

---
+++ Connections/ConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionsApp.swift
+//  Connections
+//
+//  Created by George Higbie on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
